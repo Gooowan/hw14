79a6e541d4aafa0026fdc71b53359ddc13c6621d
BUNJEE <vsolomatin@kse.org.ua>
2023-10-31 19:30:39 +0200

    3

--- task3/task3_os.js
+++ task3/task3_os.js
@@ -2,13 +2,13 @@ const os = require('os');
 
 function getSystemInfo() {
     return {
+        userInfo: os.userInfo(),
         osType: os.type(),
-        osRelease: os.release(),
         architecture: os.arch(),
+        osRelease: os.release(),
         cpuInfo: os.cpus(),
-        totalMemory: os.totalmem(),
         freeMemory: os.freemem(),
-        userInfo: os.userInfo()
+        totalMemory: os.totalmem()
     };
 }
 
